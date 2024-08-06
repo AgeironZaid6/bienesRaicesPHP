8dd486eabea12d1cdfc34bb3636dbaf95b2544e6
Felipe Velázquez <felipevelazquez2003@gmail.com>
2024-08-06 17:28:59 -0600

    HHHHHHHH

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ incluirTemplate('header', $inicio = true);
             <img src="build/img/icono1.svg" alt="Icono seguridad" loading="lazy">
             <h3>Seguridad</h3>
             <p>Possimus, suscipit repudiandae. Autem deserunt aliquid deleniti sit minus consectetur obcaecati molestiae
-                dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium. Voluptates?</p>
+                dolorem natus dolores reiciendis tempore, explicabo cum nobis laudantium Voluptates?</p>
         </div>
         <div class="icono">
             <img src="build/img/icono2.svg" alt="Icono Precio" loading="lazy">
